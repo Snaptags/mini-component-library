4f4254e35e5fe134dbb2f7eec3c3f9401e585d0d
Markus Lasermann <markus.lasermann@ptvgroup.com>
2022-08-19 10:35:16 +0200

    docs: Add basic ProgressBar documentation

---
+++ src/components/ProgressBar/DocsLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Layout = styled.div`
+  display: flex;
+  padding-bottom: 1.5rem;
+  align-items: center;
+  > strong {
+    min-width: 5rem;
+  }
+`;
+
+const DocsLayout = ({ title, children }) => (
+  <Layout>
+    <strong>{title}</strong> {children}
+  </Layout>
+);
+
+export default DocsLayout;
